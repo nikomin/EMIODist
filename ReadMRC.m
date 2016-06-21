c7d8f16b2b1aa48ffbcae84fa5e832d2770b7b0e
Niko Komin <niko.komin@uni.lu>
2016-06-21 14:47:03 +0200

    whitespaces

--- ReadMRC.m
+++ ReadMRC.m
@@ -26,31 +26,31 @@ if nargin<4
 end;
 
 % We first try for little-endian data
-f=fopen(filename,'r','ieee-le');
+f = fopen(filename,'r','ieee-le');
 if f<0
     error(['in ReadMRC the file could not be opened: ' filename])
 end;
 
 % Get the first 10 values, which are integers:
 % nc nr ns mode ncstart nrstart nsstart nx ny nz
-a=fread(f,10,'*int32');
+a = fread(f,10,'*int32');
 
 if abs(a(1))>1e5  % we must have the wrong endian data.  Try again.
     fclose(f);
-    f=fopen(filename,'r','ieee-be');
-    a=fread(f,10,'int32');  % convert to doubles
+    f = fopen(filename,'r','ieee-be');
+    a = fread(f,10,'int32');  % convert to doubles
 end;
 
 if test
     a(1:10)
 end;
 
-mode=a(4);
+mode = a(4);
 
 % Get the next 6 (entries 11 to 17), which are floats.
 % the first three are the cell dimensions, the second three are
 % angles.
-[b,cnt]=fread(f,6,'float32');
+[b,cnt] = fread(f,6,'float32');
 if test
    b
 end;
@@ -71,9 +71,9 @@ s.maps = aux1(3);
 
 % next 3 floats: min, max and mean pixel value
 aux2 = fread(f, 3, 'float32');
-mi=aux2(1); % minimum value
-ma=aux2(2); % maximum value
-av=aux2(3);  % average value
+mi = aux2(1); % minimum value
+ma = aux2(2); % maximum value
+av = aux2(3);  % average value
 
 % get 2 ints,
 aux3 = fread(f, 2, 'int32');
@@ -85,16 +85,16 @@ s.creatid = fread(f, 1, 'int16');
 
 % get the next 30 bytes
 % extra data  (not used by IMOD, first two bytes should be 0)
-[c,cnt]=fread(f,30,'char');
+[c,cnt] = fread(f,30,'char');
 
 % the next two are supposed to be character strings.
-[d,cnt]=fread(f,2,'int16');
+[d,cnt] = fread(f,2,'int16');
 s.nint = d(1);
 s.nreal = d(2);
 
 % get the next 20 bytes
 % extra data  (not used by IMOD)
-[aux4,cnt]=fread(f,20,'char');
+[aux4,cnt] = fread(f,20,'char');
 
 % get 2 ints,
 aux5 = fread(f, 2, 'int32');
@@ -108,62 +108,63 @@ aux8 = fread(f, 1, 'float32');
 
 % 10 strings of 80 characters each
 for i=1:10
-	[g,cnt]=fread(f,80,'char');
-	str(i,:)=char(g)';
+	[g,cnt] = fread(f,80,'char');
+	str(i,:) = char(g)';
 end;
 
 % disp('header:'); disp(' ');
 % disp(str(1:ns,:));
 % disp(' ');
-s.header=str;
+s.header = str;
 
 % Get ready to read the data.
-s.nx=double(a(1));
-s.ny=double(a(2));
-s.nz=double(a(3));
+s.nx = double(a(1));
+s.ny = double(a(2));
+s.nz = double(a(3));
 % Grid size in X, Y, and Z
-s.mx=a(8);
-s.my=a(9);
-s.mz=a(10);
+s.mx = a(8);
+s.my = a(9);
+s.mz = a(10);
 switch mode
     case 0
-        string='*uint8';
-        pixbytes=1;
+        string = '*uint8';
+        pixbytes = 1;
     case 1
-        string='*int16';
-        pixbytes=2;
+        string = '*int16';
+        pixbytes = 2;
     case 2
-        string='*float32';
-        pixbytes=4;
+        string = '*float32';
+        pixbytes = 4;
     case 6
-        string='*uint16';
+        string = '*uint16';
     otherwise
         error(['ReadMRC: unknown data mode: ' num2str(mode)]);
-        string='???';
-        pixbytes=0;
+        string = '???';
+        pixbytes = 0;
 end;
 
 if(s.next>0)
-    [ex_header,cnt]=fread(f,s.next,'char');
+    [ex_header,cnt] = fread(f,s.next,'char');
     disp(['Read extra header of ',num2str(c(2)),' bytes!'])
 %    disp((ex_header'));
 end
 
-skipbytes=0;
-nz=s.nz;
+skipbytes = 0;
+nz = s.nz;
 if startSlice>1
-    skipbytes=(startSlice-1)*s.nx*s.ny*pixbytes;
+    skipbytes = (startSlice-1) * s.nx * s.ny * pixbytes;
     fseek(f,skipbytes,'cof');
-    nz=min(s.nz-(startSlice-1),numSlices);
+    nz = min(s.nz-(startSlice-1),numSlices);
 end;
-ndata=s.nx * s.ny * nz;
+ndata = s.nx * s.ny * nz;
 if test
     string
     ndata
 end;
-[map,cnt]=fread(f,ndata,string);
+[map,cnt] = fread(f,ndata,string);
 fclose(f);
 if cnt ~= ndata
     error('ReadMRC: not enough data in file.');
 end;
-map=reshape(map,s.nx,s.ny,nz);
+
+map = reshape(map, s.nx, s.ny, nz);
